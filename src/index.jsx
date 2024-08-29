e083611e7319d42589d73ef80f1ec580036d6dda
amirmMRI <amirmohammadmehri14@gmail.com>
2024-08-29 21:21:56 +0330

    taghirate 8shahrivar2

--- src/index.jsx
+++ src/index.jsx
@@ -4,14 +4,14 @@ import './index.css';
 import App from './App';
 
 
-        <head>
-     <Helmet>
-        <title>لحظه کارت</title>
-        <meta name="لحظه کارت کارت هدیه ان اف تی" content="PAGE FOR DETAIL AND SHOP GIFT CARDS lahzecard" />
-        <meta name="لحظه کارت" content="لحظه کارت, لحظهکارت, گیفت,کارت هدیه,gift card,lahze,card,lahze card " />
+    //     <head>
+    //  <Helmet>
+    //     <title>لحظه کارت</title>
+    //     <meta name="لحظه کارت کارت هدیه ان اف تی" content="PAGE FOR DETAIL AND SHOP GIFT CARDS lahzecard" />
+    //     <meta name="لحظه کارت" content="لحظه کارت, لحظهکارت, گیفت,کارت هدیه,gift card,lahze,card,lahze card " />
 
-    </Helmet>
-        </head>
+    // </Helmet>
+    //     </head>
 
         const root = ReactDOM.createRoot(document.getElementById('root'));
         root.render(
