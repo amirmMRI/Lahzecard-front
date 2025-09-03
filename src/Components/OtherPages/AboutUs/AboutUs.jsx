e0072d5dcedbf60ce8fadf7b332832a78222dc33
Elahe Yaghoubi <elaheyaghoobi1@gmail.com>
2025-09-03 18:56:52 +0100

    hamkari's comment carousel translation button added. about us refactoring onboarding

--- src/Components/OtherPages/AboutUs/AboutUs.jsx
+++ src/Components/OtherPages/AboutUs/AboutUs.jsx
@@ -1,11 +1,10 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
 // Styles
-import styles from './AboutUs.module.css'
+import styles from "./AboutUs.module.css";
 
 // Images
-import AboutUspic from '../../../Images/AboutUs.png'
-import AboutUspicforphone from '../../../Images/AboutUsphone.png'
+import AboutUspic from "../../../Images/Company-amico 1.svg";
 
 // Components
 import Navbar from "../../Navbar/Navbar";
@@ -13,38 +12,51 @@ import Footer from "../../Footer/Footer";
 
 //multi language
 import t from "../../../Multilanguage";
-import {useTranslation} from 'react-i18next';
+import { useTranslation } from "react-i18next";
+
+const Divider = ({ text }) => {
+  return (
+    <div className={styles.divider_container}>
+        <div className={styles.divider_text}>{t(text)}</div>
+        <div className={styles.divider_line}></div>
+    </div>
+  );
+};
+
 
 const AboutUs = () => {
+  //------Multi Language----
+  const { t, i18n } = useTranslation();
+  let isRTL = i18n.language === "ar" || i18n.language === "ir";
+  // console.log(isRTL);
 
-    //------Multi Language----
-    const { t, i18n } = useTranslation();
-    let isRTL = i18n.language === 'ar' || i18n.language === 'ir';
-    console.log(isRTL);
+  return (
+    <div className={styles.AboutUs_Container}>
+      <section className={styles.main_qnada_sec}>
+        <section className={styles.navbar_qnda_sec}>
+          <Navbar />
+        </section>
+        <section
+          className={styles.container}
+          style={{
+            direction: isRTL ? "rtl" : "ltr",
+            textAlign: !isRTL ? "left" : "right",
+          }}
+        >
+          <div className={styles.info}>
+            <p>
+              <span>{t("lahzecard")} </span>
+              {t("aboutusparageraph")}
+            </p>
+            <img src={AboutUspic} alt="" />
+          </div>
 
-    return (
-        <div className={styles.AboutUs_Container}>
-            <section className={styles.main_qnada_sec}>
-                <section className={styles.navbar_qnda_sec}>
-                    <  Navbar/>
-                </section>
-                <section
-                    className={styles.AboutUs_info_sec}
-                    style={{direction: isRTL ? 'rtl' : 'ltr',}}
-                >
-                    <h1>{t("aboutus")}</h1>
-                    <p>{t("aboutusparageraph1")}</p>
-                    <p>{t("aboutusparageraph2")}</p>
-                    <p>{t("aboutusparageraph3")}</p>
-                </section>
-                <img src={AboutUspic} alt="info pic containing names and positions of the names."
-                     className={styles.info_pic}/>
-                <img src={AboutUspicforphone} alt="info pic containing names and positions of the names."
-                     className={styles.info_pic_for_phone}/>
-            </section>
-            <  Footer/>
-        </div>
-    );
-}
+          <Divider text="team_development" />
+        </section>
+      </section>
+      <Footer />
+    </div>
+  );
+};
 
 export default AboutUs;
